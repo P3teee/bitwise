c3a0e42e5678dca9d4404540a0415e226fb43fbf
P3teee <te14.peter.berg@umea.nti.se>
2016-11-25 11:29:51 +0100

    Cool

--- src/bitwise.java
+++ src/bitwise.java
@@ -2,5 +2,32 @@
  * Created by bepe14 on 2016-11-25.
  */
 public class bitwise {
-    
+    private short coord;
+
+    public bitwise(int x, int y) {
+        coord = 0;
+        setHigh(x);
+        setLow(y);
+    }
+
+    public void setHigh(int x) {
+        coord = (short)(coord & (x<<8));
+    }
+
+    public void setLow(int y) {
+        coord = (short)(coord & y);
+    }
+
+    public int getHigh() {
+        return (coord>>>8);
+    }
+
+    public int getLow(){
+        return (coord & 0xFF);
+    }
+
+    public static void main(String[] args) {
+        bitwise test = new bitwise(200, 200);
+        System.out.println("x = " + test.getHigh() + ", y = " + test.getLow());
+    }
 }
